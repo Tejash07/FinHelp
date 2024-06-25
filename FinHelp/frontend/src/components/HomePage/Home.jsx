6d078e904f4af565d36d97f6fb6819b2843d47c7
TEJASH RANJAN PATI <124485859+Tejash07@users.noreply.github.com>
2024-06-25 23:02:28 +0530

    Update Home.jsx

--- FinHelp/frontend/src/components/HomePage/Home.jsx
+++ FinHelp/frontend/src/components/HomePage/Home.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
-import LeftDrawer from '../ProfilePage/LeftDrawer'
-import Middle from './Middle'
-import Right from './Right'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import LeftDrawer from "../ProfilePage/LeftDrawer";
+import Middle from "./Middle";
+import Right from "./Right";
+import Footer from "./Footer";
 const Home = () => {
   return (
-    <div className=' flex bg-[#13161F]'>
-      <LeftDrawer/>
-      <Middle/>
-      <Right/>
-      
+    <div className=" flex flex-col bg-[#13161F]">
+      <div className="flex flex-row">
+        <LeftDrawer />
+        <Middle />
+        <Right />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
